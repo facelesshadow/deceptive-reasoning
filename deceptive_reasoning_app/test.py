619f3733ad7b51df9289bf8cb58194e84090480d
facelesshadow <aryanyadavsh369@gmail.com>
2025-09-11 20:30:38 +0530

    ...

--- deceptive_reasoning_app/test.py
+++
@@ -1,9 +0,0 @@
-from core.graphs import build_graph
-from langchain_google_genai import ChatGoogleGenerativeAI
-model = ChatGoogleGenerativeAI(model="gemini-2.0-flash-lite", google_api_key="...")
-
-print(model.invoke("HEY"))
-
-graph = build_graph(model)
-
-print(graph.invoke({"messages":"Can you write me a report on laptop?"}))
